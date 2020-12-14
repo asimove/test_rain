6004e49ef44bc6ffcb2be78e1410d5eb0671eebf
asimove <pascal.bras@gmail.com>
2020-12-14 13:14:03 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-function nuage (pluie: boolean) {
+function nuage (pluie: boolean, power: number) {
     if (pluie) {
         led.plot(1, 0)
         led.plot(2, 0)
@@ -9,30 +9,33 @@ function nuage (pluie: boolean) {
         led.plot(3, 2)
         led.plot(4, 1)
         led.plot(4, 2)
-        led.plot(4, 3)
-        led.plot(3, 4)
-        led.plot(2, 3)
-        led.plot(1, 4)
-        led.plot(0, 3)
-        basic.pause(600)
-        led.unplot(1, 0)
-        led.unplot(2, 0)
-        led.unplot(3, 0)
-        led.unplot(0, 1)
-        led.unplot(1, 2)
-        led.unplot(2, 2)
-        led.unplot(3, 2)
-        led.unplot(4, 1)
-        led.unplot(4, 2)
-        led.unplot(4, 3)
-        led.unplot(3, 4)
-        led.unplot(2, 3)
-        led.unplot(1, 4)
-        led.unplot(0, 3)
-        basic.pause(1000)
+        for (let index = 0; index < power; index++) {
+            // Rain
+            led.toggle(4, 3)
+            basic.pause(80)
+            led.toggle(4, 3)
+            basic.pause(80)
+            led.toggle(3, 4)
+            basic.pause(100)
+            led.toggle(3, 4)
+            basic.pause(100)
+            led.toggle(2, 3)
+            basic.pause(120)
+            led.toggle(2, 3)
+            basic.pause(120)
+            led.toggle(1, 4)
+            basic.pause(100)
+            led.toggle(1, 4)
+            basic.pause(90)
+            led.toggle(0, 3)
+            basic.pause(70)
+            led.toggle(0, 3)
+        }
+        basic.clearScreen()
+        basic.pause(1600)
     }
 }
-let RainON = false
 basic.forever(function () {
-    nuage(true)
+    basic.showNumber(envirobit.getTemperature())
+    nuage(true, 10)
 })
